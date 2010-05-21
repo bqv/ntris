b01d8eb4d64196df4ac52e04d9bc13580b051f1c
laszlo.simon@gmail.com <>
2010-05-21 22:21:10 +0200

    3D and 4D Drawing functionality mostly moved to separated files. Doxyfile added.

--- src/4dt_main.c
+++ src/4dt_main.c
@@ -38,16 +38,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <stdio.h>
 
-// Include math library.
-#include <math.h>
-
-// Include the computer gamer header.
 #include "4dt_ai.h"
-
-// Include the game engine.
 #include "4dt_eng.h"
-
 #include "4dt_g3d.h"
+#include "4dt_g4d.h"
 /*
 --------------------------------------------------------------------------------
    GLOBAL VARIABLES
@@ -68,20 +62,12 @@ static double timestep = 1.5;
 // time ellapsed while the AI steps the next turn;
 static double timestepAI = 0.15;
 
-// size of cube represent the lowest level;
-static double baseSize = 0.75;
-// size of cube represent the highest level;
-static double maxSize = 2.0;
-
 // initial direction of the viewPort (deg);
 static double angleX = -60.0;
 static double angleZ = 10.0;
 
 // changes of the viewPort direction by one keypress (deg);
 static double dangle = 10.0;
-
-// Color of the wireframe 4D cube.
-const GLfloat wire4d_color[4] = {0.4, 0.4, 0.6, 0.15};
 
 // Color of the 4D cube.
 float cube4d_color[4] = {0.4, 0.4, 0.6, 0.15};
@@ -100,65 +86,6 @@ static GLfloat level_colors[SPACELENGTH][4];
 /*------------------------------------------------------------------------------
     VISUALISATION
 */
-// Calculates perspective projection factor of the 'level'
-// (game space 4th axis).
-double perspFact(double level)
-{
-  // Local variables:
-  double baseLevel, // lowest level if the viewpoint is at zero level;
-         sizeFact,  // factor of highest and lowest level projected size;
-         result;    // return value.
-
-  // Calculate size factor.
-  sizeFact = baseSize / maxSize;
-
-  // Calculate viewpoint distance from lowest level.
-  baseLevel = (double)SPACELENGTH * sizeFact / (1 - sizeFact);
-
-  // Calculate perspective projection.
-  result = baseSize * (baseLevel + SPACELENGTH) /
-           (baseLevel + SPACELENGTH - level);
-
-  // Return with the result value.
-  return result;
-}
-
-// Calculates the crossProduct of v1 and v2 vector where
-// 'n' the result vector.
-void crossProduct(double *n, double v1[3], double v2[3])
-{
-  // Calculate crossproduct by coordinates.
-  n[0] = v1[1] * v2[2] - v2[1] * v1[2];
-  n[1] = v1[2] * v2[0] - v2[2] * v1[0];
-  n[2] = v1[0] * v2[1] - v2[0] * v1[1];
-}
-
-// Normalise a vector.
-void normalise(double *v)
-{
-  // Local variables:
-  int i; // loop counter;
-  double l; // length of the vector.
-
-  // Calculate length of the vector.
-  l = sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
-
-  // For each coordinate
-  for (i = 0; i < 3; i++)
-  {
-    // normalise the coordinate.
-    v[i] /= l;
-  }
-}
-
-// Calculate normal vector of the plane of v1, v2 vector.
-void calcNormal(double *n, double v1[3], double v2[3])
-{
-  // Calculate normal by crossproducting v1 and v2.
-  crossProduct(n, v1, v2);
-  // Normalise the normal vector.
-  normalise(n);
-}
 
 // Draw bitmap text to the specified coordinates and font.
 void renderString(double x, double y, double z, void *font, char *string)
@@ -195,162 +122,11 @@ static void writeScore(void)
   renderString(-3.5, -2.5, -6.0, GLUT_BITMAP_9_BY_15, text);
 }
 
-// Draw a 4D cube to the specified place of the game space.
-static void cube4D(double x, double y, double z, double l,
-                   float color[4],
-                   int dimension,
-                   int enable_wire)
-{
-  // Array contains the points of a 4D hypercube.
-  float points[16][4] =
-  /*  x,   y,   z,   l */
-  {{0.0, 0.0, 0.0, 0.0},
-   {1.0, 0.0, 0.0, 0.0},
-   {0.0, 1.0, 0.0, 0.0},
-   {1.0, 1.0, 0.0, 0.0},
-   {0.0, 0.0, 1.0, 0.0},
-   {1.0, 0.0, 1.0, 0.0},
-   {0.0, 1.0, 1.0, 0.0},
-   {1.0, 1.0, 1.0, 0.0},
-   {0.0, 0.0, 0.0, 1.0},
-   {1.0, 0.0, 0.0, 1.0},
-   {0.0, 1.0, 0.0, 1.0},
-   {1.0, 1.0, 0.0, 1.0},
-   {0.0, 0.0, 1.0, 1.0},
-   {1.0, 0.0, 1.0, 1.0},
-   {0.0, 1.0, 1.0, 1.0},
-   {1.0, 1.0, 1.0, 1.0}};
-
-  // Array contains the faces (specified with num. of the points)
-  // of a 4D hypercube.
-  const char faces[24][4] =
-  {
-// inner cube
-{   8,  12,  14,  10}, {   9,  11,  15,  13}, {   8,   9,  13,  12},
-{  10,  14,  15,  11}, {   8,  10,  11,   9}, {  12,  13 ,  15,  14},
-// outer cube
-{   0,   4,   6,   2}, {   1,   3,   7,   5}, {   0,   1,   5,   4},
-{   2,   6,   7,   3}, {   0,   2,   3,   1}, {   4,   5,   7,   6},
-// intermediate faces
-{   0,   8, 0xC,   4}, {   0,   8, 0xA,   2}, { 0xA,   2,   6, 0xE}, { 0xE,   6,   4, 0xC},
-{   1,   9, 0xB,   3}, { 0xB,   3,   7, 0xF}, {   7, 0xF, 0xD,   5}, { 0xD,   5,   1,   9},
-{ 0xC, 0xD,   5,   4}, {   8,   9,   1,   0}, { 0xA, 0xB,   3,   2}, { 0xE, 0xF,   7,   6}
-  };
-
-  // Normal vector of the actual facet.
-  double norm[3];
-  // Vectors of two edge of the facet.
-  double v1[3];
-  double v2[3];
-
-  // Loop counter.
-  int n, i, j, k;
-  int facenum;
-
-  // For each point of the hypercube
-  for (n = 0; n < 16; n++)
-  {
-      // correct the coordinates with the perspective torsion to
-      // the direction of the 4th axis.
-      points[n][0] = (points[n][0] + x) * perspFact(l - (n < 8));
-      points[n][1] = (points[n][1] + y) * perspFact(l - (n < 8));
-      points[n][2] = (points[n][2] + z) * perspFact(l - (n < 8));
-  }
-
-  facenum = (dimension == 3) ? 6 : 24;
-
-  // For each facet and
-  for (i = 0; i < facenum; i++)
-  {
-    float pointlist[4][4];
-
-    for (k = 0; k < 4; k++)
-    for (j = 0; j < 4; j++)
-    {
-      pointlist[k][j] = points[faces[i][k]][j];
-    }
-
-    // For each coordinate
-    for (k = 0; k < 3; k++)
-    {
-      // calculate two vector of two edge from the points.
-      v1[k] = points[faces[i][1]][k] - points[faces[i][0]][k];
-      v2[k] = points[faces[i][3]][k] - points[faces[i][0]][k];
-    }
-
-    // Calculate normal vector.
-    calcNormal(norm, v1, v2);
-
-    // Set the color of the facet.
-    glColor4d(color[0], color[1], color[2], color[3]);
-
-    // Start draw a quad.
-    glBegin(GL_QUADS);
-    // Set the normal vector.
-    glNormal3d(norm[0], norm[1], norm[2]);
-
-    // For each point of the facet
-    for (k = 0; k < 4; k++)
-      // set the points of the quad.
-      glVertex3d(points[faces[i][k]][0],
-                 points[faces[i][k]][1],
-                 points[faces[i][k]][2]);
-    // Finish drawing.
-    glEnd();
-
-    if (enable_wire)
-    {
-      // Set the color of the facet's wireframe.
-      glColor4d(wire4d_color[0], wire4d_color[1], wire4d_color[2], wire4d_color[3]);
-
-      // Start draw a line strip.
-      glBegin(GL_LINE_STRIP);
-        // For each point of the facet
-      for (k = 0; k < 4; k++)
-        // set the points of the strip.
-        glVertex3d(points[faces[i][k]][0],
-                   points[faces[i][k]][1],
-                   points[faces[i][k]][2]);
-
-      glVertex3d(points[faces[i][0]][0],
-                 points[faces[i][0]][1],
-                 points[faces[i][0]][2]);
-      // Finish drawing.
-      glEnd();
-    }
-  }
-}
-
 // Main drawing function.
 static void display(void)
 {
   // Local variables:
   int l, x, y, z;        // loop counter;
-
-  // Time storage for lower down the solid and AI step
-  // initialised with ellapsed time since program started.
-  static double timeLower = 0.0;
-  static double timeAI = 0.0;
-
-  // Time ellapsed since program started.
-  const double t = glutGet(GLUT_ELAPSED_TIME) / 1000.0;
-
-  // If time ellapsed since last storage larger the time of step down,
-  if (t - timeLower > timestep) {
-    // lower the solid and
-    lowerSolid();
-    // store the actual time.
-    timeLower = t;
-  }
-
-  // If time ellapsed since last storage larger the time of AI step,
-  // and auto player is switched on,
-  if ((t - timeAI > timestepAI) && (autoGamer_ON)) {
-    // make the AI step and
-    dostep();
-    // store the actual time.
-    timeAI = t;
-  }
 
   // Clear the display buffer.
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -364,10 +140,6 @@ static void display(void)
   glTranslated(0, 0, -6);
   glRotated(angleX, 1, 0, 0);
   glRotated(angleZ, 0, 0, 1);
-
-  glDepthMask(GL_TRUE);
-//  glEnable(GL_LIGHTING);
-  glDisable(GL_BLEND);
 
   // Draw the gamespace.
 
@@ -383,14 +155,10 @@ static void display(void)
       if (!!getSpaceCell(l, x, y, z))
       {
         // draw the cube.
-        cube4D(x - 1, y - 1, z - 1, 1 + l, level_colors[l], 4, 0);
+        g4dDraw4DCube(x - 1, y - 1, z - 1, 1 + l, level_colors[l], 4, 0);
       }
     }
   }
-
-  glDepthMask(GL_FALSE);
-//  glDisable(GL_LIGHTING);
-  glEnable(GL_BLEND);
 
   // Draw the bottom level.
 
@@ -399,7 +167,7 @@ static void display(void)
   for (y = 0; y <= 1; y++)
   for (z = 0; z <= 1; z++)
   {
-    cube4D(x - 1, y - 1, z - 1, 0, cube4d_color, 3, 1);
+    g4dDraw4DCube(x - 1, y - 1, z - 1, 0, cube4d_color, 3, 1);
   }
 
   // Draw the actual solid.
@@ -415,14 +183,10 @@ static void display(void)
       if (!!getSolidCell(l, x, y, z))
       {
         // draw the hypercube.
-        cube4D(x - 1, y - 1, z - 1, ge.pos + l, cube4d_color, 4, 1);
+        g4dDraw4DCube(x - 1, y - 1, z - 1, ge.pos + l, cube4d_color, 4, 1);
       }
     }
   }
-
-  glDepthMask(GL_TRUE);
-  glEnable(GL_LIGHTING);
-  glDisable(GL_BLEND);
 
   glPopMatrix();
 
@@ -547,6 +311,32 @@ static void special(int key, int x, int y)
 // Idle process.
 static void idle(void)
 {
+
+  // Time storage for lower down the solid and AI step
+  // initialised with ellapsed time since program started.
+  static double timeLower = 0.0;
+  static double timeAI = 0.0;
+
+  // Time ellapsed since program started.
+  const double t = glutGet(GLUT_ELAPSED_TIME) / 1000.0;
+
+  // If time ellapsed since last storage larger the time of step down,
+  if (t - timeLower > timestep) {
+    // lower the solid and
+    lowerSolid();
+    // store the actual time.
+    timeLower = t;
+  }
+
+  // If time ellapsed since last storage larger the time of AI step,
+  // and auto player is switched on,
+  if ((t - timeAI > timestepAI) && (autoGamer_ON)) {
+    // make the AI step and
+    dostep();
+    // store the actual time.
+    timeAI = t;
+  }
+
   // Refresh the display.
   glutPostRedisplay();
 }
@@ -591,6 +381,8 @@ int main(int argc, char *argv[])
   initLevelColors();
 
   g3dInit(argc, argv);
+
+  g4dCalibrate(SPACELENGTH);
 
   // Set the display function.
   glutDisplayFunc(display);
