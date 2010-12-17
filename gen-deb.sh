0d6a9b242f17c11f0d864b537d131cb52ab97f89
Simon Laszlo <laszlo.simon@gmail.com>
2010-12-17 15:16:05 +0100

    New languages added, deb build-dep fixed

--- gen-deb.sh
+++ gen-deb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-DEBEMAIL=laszlo.simon@gmail.com
-DEBFULLNAME=Laszlo Simon
+DEBEMAIL="laszlo.simon@gmail.com"
+DEBFULLNAME="Laszlo Simon"
 
 dh_make -c gpl3 -n -s -e $DEBEMAIL
 
@@ -8,7 +8,8 @@ mv debian/control debian/control.bak
 perl -pe 's/Section:.*/Section: games/; \
           s|Homepage:.*|Homepage: https://launchpad.net/4dtris|; \
           s/Description:.*/Description: Tetris like game in 4D/; \
-          s/ <.*/ This Application is a Tetris game extended to 4 dimension using OpenGL and GLUT./;' \
+          s/Build-Depends:.*/Build-Depends: debhelper (>= 7), autotools-dev, libtool, gettext, libsdl1.2-dev, libsdl-ttf2.0-dev, libgl1-mesa-dev, libglu1-mesa-dev/; \
+          s/^ <.*/ This Application is a Tetris game extended to 4 dimension using OpenGL and GLUT./;' \
   <debian/control.bak >debian/control
 
 mv debian/menu.ex debian/menu.bak
